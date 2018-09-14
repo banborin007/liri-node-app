85ecb0c7b789ea9154f1de1d974d9c3b0c2b0cd4
Bo <ban.borin@yahoo.com>
2018-09-14 09:59:49 -0700

    a work in progress

---
+++ liri.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+
+require("dotenv").config();
+   var Spotify = require('node-spotify-api');
+   var keys = require('./keys.js');
+
+   var spotify = new Spotify(keys.spotify);
+console.log(spotify);
+
+module.imports
