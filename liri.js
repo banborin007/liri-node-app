e1c351526bdb2c9dfe5cdf19ed755771015d5f99
Bo <ban.borin@yahoo.com>
2018-09-16 16:47:10 -0700

    modified .gitignore, added dependencies, linked key.js to liri, gave options and selection mode, still in progress

--- liri.js
+++ liri.js
@@ -1,12 +1,36 @@
 require("dotenv").config();
-
+var Spotify = require('node-spotify-api');
+var keys = require('./keys.js');
 var spotify = new Spotify(keys.spotify);
+var inquirer = require("inquirer");
 
-require("dotenv").config();
-   var Spotify = require('node-spotify-api');
-   var keys = require('./keys.js');
+var option1 = "";
+var option2 = "";
+inquirer.prompt([{
+    type: "list",
+    message: "Choose: ",
+    choices: ["Concert", "Spotify", "Movie", "do this"],
+    name: "a"
+}, {
+    type: "input",
+    message: "Search:",
+    name: "b"
+}
+]).then(answers => {
+    // switch case statements
+    option1 = answers.a;
+    option2 = answers.b;
+    spotifyThis();
+});
 
-   var spotify = new Spotify(keys.spotify);
-console.log(spotify);
+function spotifyThis() {
+    var songName = option2;
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log(JSON.stringify(data.tracks.items[0].name)); 
+      });
 
-module.imports
+}
